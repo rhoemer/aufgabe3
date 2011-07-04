04156bef329b72fd5c6b918b23aab24dd01da67f
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:32:35 -0700

    Edited routefinder.rb via GitHub

--- routefinder.rb
+++ routefinder.rb
@@ -3,52 +3,59 @@ require 'Graph'
 
 
 class RouteFinder
-	attr_accessor :route, :block, :success, :strecke
+	attr_accessor :route, :success, :strecke, :block
 	
 
 
 	def initialize(initRoute)
 	    g = Graph.new("AB6,BC3,CE5,EC8,CE3,BE2,BD5,DE7") 	
 		@strecke = 0												
-		@route = initRoute.split('-')							
-		@block = []									
+		
+                @route = initRoute.split('-')							
+		
+                @block = []									
 															
-		x = 0
-		while x < route.size - 1							
-			@block.insert(x, @route[x] + @route[x+1])			
-			x +=1
+		i = 0
+		while i < route.size - 1							
+			@block.insert(i, @route[i] + @route[i+1])			
+			
+                        i +=1
 		end
 
-		x = 0	
-		while x < block.size									
-			y = 0
-			while y < g.stationen.size
-				if @block[x] == g.stationen[y]				
-					@strecke = @strecke + g.kanten[y].to_i		 
+		i = 0	
+		while i < block.size									
+			j = 0
+			while j < g.stationen.size
+				if @block[i] == g.stationen[j]				
+					@strecke = @strecke + g.kanten[j].to_i		 
 					@success = true								
 				
                                         break if success == true
 				else
 					@success = false							
 				end			
-				y +=1
+				j +=1
 			end
 			break if success == false							
-			x +=1	
+			i +=1	
 		end
 	end
 			
 	def to_s
 		if success 												
-		puts "Strecke insgesamt:"									
+		puts "Strecke Betrag:"									
 		@strecke
-		elsif 
+		
+                elsif 
 		puts "Keine Verbindung verfügbar"									
                 end
 	end
 
 end
 
+
 n = RouteFinder.new("A-B-C")
+
+
 puts n.to_s														
 
