cb018cb3794e62ec3b4dac6f1b7c4dcd59642427
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:26:47 -0700

    Edited graph.rb via GitHub

--- graph.rb
+++ graph.rb
@@ -2,24 +2,20 @@ class Graph
 	attr_accessor :stationen, :kanten
 	
 	def initialize(initKanten)
-	
-		@kanten = initKanten.split(',')								
-		
-
-                @stationen = initKanten.split(',')
-		
-		y = 0
-			while y < kanten.size
-				stationen[y] = stationen[y].delete "1234567890"				
-			y = y+1
+	        @kanten = initKanten.split(',')								
+	        @stationen = initKanten.split(',')
+		i = 0
+			while i < kanten.size
+				stationen[i] = stationen[i].delete "1234567890"				
+		                i = i+1
 			end	
 		
-		x = 0
+		z = 0
 			
 
-                                while x < kanten.size			
-				kanten[x] = kanten[x].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
-				x = x+1
+                                while z < kanten.size			
+				kanten[z] = kanten[z].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
+				z = z+1
 			end	
 	end
 	
