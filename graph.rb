6e6c4049dfbaaf508061caf585368342c334edf8
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:56:00 -0700

    Edited graph.rb via GitHub

--- graph.rb
+++ graph.rb
@@ -11,20 +11,22 @@ class Graph
 	        
 		
                 i = 0
-			while i < kanten.size
-				knoten[i] = knoten[i].delete "1234567890"				
-		                i = i + 1
-			end	
+		while i < kanten.size
+			knoten[i] = knoten[i].delete "1234567890"				
+		        i = i + 1
+		end	
 		
-		                z = 0
-			        while z < kanten.size			
-				kanten[z] = kanten[z].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
-				z = z + 1
-			        end	
+		z = 0
+	        while z < kanten.size			
+		kanten[z] = kanten[z].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
+		z = z + 1
+		end	
 	end
 	
-                def to_s
-		@kanten + @knoten
+        
+
+        def to_s
+                @kanten + @knoten
 	end
 end
 
