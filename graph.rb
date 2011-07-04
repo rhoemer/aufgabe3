e943ef158821199b841b91fe7d015bb19902523a
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:12:15 -0700

    Edited graph.rb via GitHub

--- graph.rb
+++ graph.rb
@@ -3,18 +3,22 @@ class Graph
 	
 	def initialize(initKanten)
 	
-		@kanten = initKanten.split(',')										#Zu erst werden beide Arrays mit den übergebenen Verbindungen gefüllt und von den Kommas befreit
-		@stationen = initKanten.split(',')
+		@kanten = initKanten.split(',')								
+		
+
+                @stationen = initKanten.split(',')
 		
 		y = 0
 			while y < kanten.size
-				stationen[y] = stationen[y].delete "1234567890"				#Um ein Array für die Verbindungen zu kriegen, werden einfach alle Zahlen entfernt
+				stationen[y] = stationen[y].delete "1234567890"				
 			y = y+1
 			end	
 		
 		x = 0
-			while x < kanten.size			
-				kanten[x] = kanten[x].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	#Das gleiche nur umgekehrt wird gemacht um ein Array mit der Strecke zwischen den Knoten zu erhalten
+			
+
+                                while x < kanten.size			
+				kanten[x] = kanten[x].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
 				x = x+1
 			end	
 	end
