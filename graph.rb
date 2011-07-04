c794f534385f79388dab4c9dbfb8dc99d0a97e62
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:37:41 -0700

    Edited graph.rb via GitHub

--- graph.rb
+++ graph.rb
@@ -1,32 +1,30 @@
 class Graph
-	attr_accessor :stationen, :kanten
+	attr_accessor :knoten, :kanten
 	
 
 
 
 	def initialize(initKanten)
 	        @kanten = initKanten.split(',')								
-	        @stationen = initKanten.split(',')
+	        @knoten = initKanten.split(',')
 		i = 0
 			while i < kanten.size
-				stationen[i] = stationen[i].delete "1234567890"				
+				knoten[i] = knoten[i].delete "1234567890"				
 		                i = i+1
 			end	
 		
-		z = 0
-			
-
-                                while z < kanten.size			
+		                z = 0
+			        while z < kanten.size			
 				kanten[z] = kanten[z].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
 				z = z+1
-			end	
+			        end	
 	end
 	
 
 
 	
 	def to_s
-		@kanten + @stationen
+		@kanten + @knoten
 	end
 
 end
