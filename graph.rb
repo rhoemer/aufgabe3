10febcc0f24e1882dfe6785940968d94c8cd74c9
rhoemer <rhoemer@hotmail.de>
2011-07-04 10:42:11 -0700

    Edited graph.rb via GitHub

--- graph.rb
+++ graph.rb
@@ -5,18 +5,19 @@ class Graph
 
 
 	def initialize(initKanten)
-	        @kanten = initKanten.split(',')								
 	        @knoten = initKanten.split(',')
+                @kanten = initKanten.split(',')								
+	        
 		i = 0
 			while i < kanten.size
 				knoten[i] = knoten[i].delete "1234567890"				
-		                i = i+1
+		                i = i + 1
 			end	
 		
 		                z = 0
 			        while z < kanten.size			
 				kanten[z] = kanten[z].delete "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
-				z = z+1
+				z = z + 1
 			        end	
 	end
 	
